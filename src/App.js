33d2e082f3b9ef6d1b14106dacca230a23ab64cf
Bosiipo <bosiposhogbolu@gmail.com>
2021-01-16 22:01:21 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import style from './App.module.css';
 // import InfiniteScroll from 'react-infinite-scroll-component';
 import { Container, Row, Col, Spinner } from "reactstrap";
--- src/components/Data/index.js
+++ src/components/Data/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './data.css';
 
 const Data = ({user}) => {
